a425f3e1584095a0dc51309c770c5f5e808f6098
Michael Kachi <jesmikky@gmail.com>
2023-03-05 14:30:23 +0100

    structure changed

---
+++ app/resources/views/layouts/app.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html class="no-js" lang="zxx">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+    <title> {{$settings->site_name}}</title>
+
+    <!--== Favicon ==-->
+    <link rel="shortcut icon" href="{{asset('/asset/img/favicon.ico')}}" type="image/x-icon"/>
+
+    <!--== Google Fonts ==-->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,600,700" rel="stylesheet"/>
+
+   
+    <link href="{{asset('/assets/css/style.css')}}" rel="stylesheet"/>
+
+    <link href="{{asset('/assets/css/slicknav.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/slick.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/settings.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/pe-7-stroke-icon.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/nice-select.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/magnific-popup.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/leaflet.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/jquery-ui.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/helper.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/font-awesome.min.css')}}" rel="stylesheet"/>
+    <link href="{{asset('/assets/css/bootstrap.min.css')}}" rel="stylesheet"/>
+    <!--== Main Style CSS ==-->
+    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet"/>
+
+    <!--[if lt IE 9]>
+    <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+   @include('layouts.header')
+    @yield('contents')
+
+<!--== Start Footer Area Wrapper ==-->
+@include('layouts.footer')
+<!-- End Off Canvas Menu Wrapper -->
---
+++ app/resources/views/layouts/js.blade.php
@@ -0,0 +1,33 @@
+
+<!--=======================Javascript============================-->
+<!--=== All Vendor Js ===-->
+<script src="{{asset('/assets/js/vendors/modernizr.min.js')}}"></script>
+<script src="{{asset('/assets/js/vendors/jquery.min.js')}}"></script>
+<script src="{{asset('/assets/js/vendors/jquery-migrate.min.js')}}"></script>
+<script src="{{asset('/assets/js/vendors/bootstrap.min.js')}}"></script>
+<script src="{{asset('/assets/js/plugins/headroom.min.js')}}"></script>
+<script src="{{asset('/assets/js/plugins/hoverdir.min.js')}}"></script>
+<script src="{{asset('/assets/js/plugins/isotope.pkgd.min.js')}}"></script>
+<script src="{{asset('/assets/js/plugins/jquery.counterup.min.js')}}"></script>
+<script src="{{asset('/assets/js/plugins/jQuery.headroom.js')}}"></script>
+<script src="{{asset('/assets/js/plugins/jquery.magnific-popup.min.js')}}"></script>
+<script src="{{asset('/assets/js/plugins/jquery.nice-select.min.js')}}"></script>
+<script src="{{asset('/assets/js/plugins/jquery.slicknav.min.js')}}"></script>
+<script src="{{asset('/assets/js/plugins/jquery-ui.min.js')}}"></script>
+<script src="{{asset('/assets/js/plugins/leaflet.min.js')}}"></script>
+<script src="{{asset('/assets/js/plugins/slick.min.js')}}"></script>
+<script src="{{asset('/assets/js/plugins/tippy.all.min.js')}}"></script>
+<script src="{{asset('/assets/js/plugins/waypoints.min.js')}}"></script>
+<!--=== Active Js ===-->
+<script src="{{asset('/assets/js/active.js')}}"></script>
+
+<!-- REVOLUTION JS FILES -->
+<script src="{{asset('/assets/js/revslider/jquery.themepunch.tools.min.js')}}"></script>
+<script src="{{asset('/assets/js/revslider/jquery.themepunch.revolution.min.js')}}"></script>
+
+
+<!--=== REVOLUTION JS ===-->
+<script src="{{asset('/assets/js/revslider/rev-active.js')}}"></script>
+
+</body>
+</html>
