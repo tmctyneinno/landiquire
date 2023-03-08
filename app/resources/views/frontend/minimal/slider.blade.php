49b327bea0b5833e6dd410e346a6bd6aecea06a5
Michael Kachi <jesmikky@gmail.com>
2023-03-08 03:09:48 +0100

    updates

--- app/resources/views/frontend/minimal/slider.blade.php
+++ app/resources/views/frontend/minimal/slider.blade.php
@@ -4,8 +4,10 @@
          <div id="rev_slider_1_1" class="rev_slider fullwidthabanner" data-version="5.4.7">
              <ul>
                  <!-- SLIDE 01 -->
+                 @php $x = 1; $y = 2; @endphp
                  @forelse ($sliders as  $slider)
-                 <li data-index="rs-1" data-transition="random" data-slotamount="default" data-hideafterloop="0"
+                
+                 <li data-index="rs-{{$x}}" data-transition="random" data-slotamount="default" data-hideafterloop="0"
                      data-hideslideonmobile="off" data-easein="default" data-easeout="default" data-masterspeed="default"
                      data-thumb="{{asset('images/'.$slider->image)}}" data-rotate="0"
                      data-saveperformance="off" data-title="Slide">
@@ -16,7 +18,7 @@
                      <!-- LAYERS -->
  
                      <!-- LAYER NR. 2 -->
-                     <div class="tp-caption tp-resizeme" id="slide-1-layer-2" data-x="['{{$slider->align}}','{{$slider->align}}','{{$slider->align}}','{{$slider->align}}']"
+                     <div class="tp-caption tp-resizeme" id="slide-{{$x}}-layer-{{$y}}" data-x="['{{$slider->align}}','{{$slider->align}}','{{$slider->align}}','{{$slider->align}}']"
                           data-hoffset="['10','50','10','15']" data-y="['middle','middle','middle','middle']"
                           data-voffset="['-60','-60','-90','-76']" data-width="none" data-height="none"
                           data-whitespace="nowrap" data-type="text" data-responsive_offset="on"
@@ -27,7 +29,7 @@
                      </div>
  
                      <!-- LAYER NR. 3 -->
-                     <h1 class="tp-caption   tp-resizeme" id="slide-1-layer-3" data-x="['{{$slider->align}}','{{$slider->align}}','{{$slider->align}}','{{$slider->align}}']"
+                     <h1 class="tp-caption   tp-resizeme" id="slide-{{$x}}-layer-{{$y+1}}" data-x="['{{$slider->align}}','{{$slider->align}}','{{$slider->align}}','{{$slider->align}}']"
                          data-hoffset="['10','50','10','15']" data-y="['middle','middle','middle','middle']"
                          data-voffset="['0','0','-30','-30']" data-fontsize="['70','70','70','40']"
                          data-lineheight="['70','70','70','40']" data-width="none" data-height="none"
@@ -35,21 +37,21 @@
                          data-frames='[{"delay":50,"speed":1000,"frame":"0","from":"x:-50px;opacity:0;","to":"o:1;","ease":"Power2.easeOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"Power2.easeIn"}]'
                          data-textAlign="['left','left','left','left']" data-paddingtop="[0,0,0,0]"
                          data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]">
-                         {{$slider->title}}</h1>
+                         {!! $slider->title !!}</h1>
  
                      <!-- LAYER NR. 4 -->
-                     <div class="tp-caption   tp-resizeme" id="slide-1-layer-4" data-x="['{{$slider->align}}','{{$slider->align}}','{{$slider->align}}','{{$slider->align}}']"
+                     <div class="tp-caption   tp-resizeme" id="slide-{{$x}}-layer-{{$y+2}}" data-x="['{{$slider->align}}','{{$slider->align}}','{{$slider->align}}','{{$slider->align}}']"
                           data-hoffset="['10','50','10','15']" data-y="['middle','middle','middle','middle']"
                           data-voffset="['61','61','31','14']" data-width="none" data-height="none"
                           data-whitespace="nowrap" data-type="text" data-responsive_offset="on"
                           data-frames='[{"delay":500,"speed":2000,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[100%];s:inherit;e:inherit;","to":"o:1;","ease":"Power2.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"opacity:0;","ease":"nothing"}]'
                           data-textAlign="['left','left','left','left']" data-paddingtop="[0,0,0,0]"
                           data-paddingright="[0,0,0,0]" data-paddingbottom="[0,0,0,0]" data-paddingleft="[0,0,0,0]">
-                         {{$slider->content}}
+                         {!! $slider->content !!}
                      </div>
  
                      <!-- LAYER NR. 5 -->
-                     <div class="tp-caption bttn-primary-bg rev-btn  tp-resizeme  transit-md" id="slide-1-layer-8"
+                     <div class="tp-caption bttn-primary-bg rev-btn  tp-resizeme  transit-md" id="slide-{{$x}}-layer-{{$y + 6}}"
                           data-x="['{{$slider->align}}','center','{{$slider->align}}','{{$slider->align}}']" data-hoffset="['11','-378','11','16']"
                           data-y="['top','top','middle','middle']" data-voffset="['392','391','89','75']"
                           data-fontsize="['15','16','16','16']" data-fontweight="['700','600','600','600']"
@@ -61,6 +63,11 @@
                         <a href="{{route('pages', encrypt(5))}}" style="color:#fff"> Learn More</a> 
                      </div>
                  </li>
+                 @php  
+                    $x = $x + 1;
+
+                    print_r($x);
+                 @endphp
                  @empty
                  @endforelse
              </ul>
