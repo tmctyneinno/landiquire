068daeeb314a2ad6a4a74d2d575555d54f75839b
Michael Kachi <jesmikky@gmail.com>
2023-05-17 11:46:57 +0100

    updates

--- app/resources/views/layouts/footer.blade.php
+++ app/resources/views/layouts/footer.blade.php
@@ -13,7 +13,7 @@
                                 <a href="{{$settings->facebook}}"><i class="fa fa-facebook"></i></a>
                             <a href="{{$settings->twitter}}"><i class="fa fa-twitter"></i></a>
                             {{-- <a href="{{$settings->linkedIn}}"><i class="fa fa-linkedin"></i></a> --}}
-                            <a href="{{$settings->intagram}}"><i class="fa fa-instagram"></i></a>
+                            <a href="{{$settings->instagram}}"><i class="fa fa-instagram"></i></a>
                             {{-- <a href="{{$settings->pinterest}}"><i class="fa fa-pinterest"></i></a> --}}
                             </div>
                         </div>
