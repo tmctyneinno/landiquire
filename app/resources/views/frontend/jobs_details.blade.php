d999d9a0b40317857d51f636bbdeac1d65c027e6
Michael Ozoudeh <jesmikky@gmail.com>
2023-07-19 12:46:26 +0100

    removed list style

--- app/resources/views/frontend/jobs_details.blade.php
+++ app/resources/views/frontend/jobs_details.blade.php
@@ -31,37 +31,40 @@
     <div class="about-us-page-area">
         <div class="container">
             <div class="row">
+                
                 <div class="col-lg-4 order-1 ">
                     <!-- Start Service Details Sidebar -->
                     <div class="service-details-sidebar mtm-40 mtm-sm-2 mtm-md-2">
                         <!-- Start Sidebar Item -->
-                        <div class="sidebar-single" style="background: #fff">
-                            <h3 class="sidebar-heading">Job Ids</h3>
-                            <hr>
-                            <div class="sidebar-body">
-                                @forelse ($jobs as $jo )
-                                <div class="col-md-12 p-3 mb-3" style="border: 1px solid #b2b2b260; border-radius:10px">
-                                    <div class="discover-item">
-                                        <div class="discover-item__thumb">
-                                            <img src="{{asset('/assets/img/partner/partner-1.jpg')}}" alt="Discover"/>
+                        <div class="sidebar-body">
+                            <div class="partner-content-wrap mt-50 mt-sm-40">
+                                <div class="row mtm-30">
+                                   
+                                    @forelse ($jobs as $jo )
+                                    
+                                    <div class="col-md-12 p-3 mb-3" style="border: 1px solid #b2b2b260; border-radius:10px">
+                                        <div class="discover-item">
+                                            <div class="discover-item__thumb">
+                                                <img src="{{asset('/assets/img/partner/partner-1.jpg')}}" alt="Discover"/>
+                                            </div>
+                                            <div class="discover-item__info">
+                                                {{-- <span style="float:right"> Posted: {{$jo->created_at->diffForHumans()}}</span> --}}
+                                                <h6 style="color:#0099ff">{{$jo->title}}</h6> 
+                                                <p style="color:#0099ff">{{$jo->company}}</p>
+                                                <span  class="p-1" style="border-radius: 4px; background:#9ab6c957; color:#5f5a5a"> {{$jo->location}}</span>   <span class="p-1" style="border-radius: 4px; background:#9ab6c957; color:#5f5a5a"> {{$job->job_type}}</span>  
+                                                <span class="p-1" style="border-radius: 4px; background:#9ab6c957; color:#5f5a5a"> ₦{{number_format($jo->min_salary,2)}} </span> <br>
+                                               <span> Job Function: {{$jo->industry->name}}</span> <br>
+                                               <hr>
+                                               <span> {!! substr($jo->job_details,0,200) !!} <br> <a href="{{route('job-details', $jo->id.'-'.$jo->title)}}" class=" btn-primary btn-sm"> Apply for this Job</a></span>
+                                            </div>
+
                                         </div>
-                                        <div class="discover-item__info">
-                                            {{-- <span style="float:right"> Posted: {{$job->created_at->diffForHumans()}}</span> --}}
-                                            <h6 style="color:#0099ff">{{$jo->title}}</h6> 
-                                            <p style="color:#0099ff">{{$jo->company??$jo->company}}</p>
-                                            <span  class="p-1" style="border-radius: 4px; background:#9ab6c957; color:#5f5a5a"> {{$jo->location}}</span>   
-                                            <span class="p-1" style="border-radius: 4px; background:#9ab6c957; color:#5f5a5a"> {{$jo->job_type}}</span>  
-                                             <span class="p-1" style="border-radius: 4px; background:#9ab6c957; color:#5f5a5a"> {{$jo->salary_range}}</span> <br>
-                                           <span> Job ID: {{$jo->industry->name}}</span> <br>
-                                           <hr>
-                                           <span> {!! substr($jo->job_details,0,200) !!} <br> 
-                                            <a href="{{route('job-details', $job->id.'-'.$jo->title)}}" class=" btn-primary btn-sm rounded"> Apply for this Job</a></span>
-                                        </div>
-                                      
                                     </div>
+                                    @empty
+                                    @endforelse
+
+                                    
                                 </div>
-                                @empty
-                                @endforelse
                             </div>
                         </div>
                         <!-- End Sidebar Item -->
