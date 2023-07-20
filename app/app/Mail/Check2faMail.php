12f6646fdf1f811c1d14ee5666488057687b4200
Michael Ozoudeh <jesmikky@gmail.com>
2023-07-21 00:13:58 +0100

    updates

--- app/app/Mail/Check2faMail.php
+++ app/app/Mail/Check2faMail.php
@@ -35,7 +35,7 @@ class Check2faMail extends Mailable
     {
         return new Envelope(
             subject: $this->data['subject'],
-            from: new Address('support@mazeoptions.com', 'NCIC World'),
+            from: new Address('jobs@ncicworld.com', 'NCIC World'),
         );
     }
 
